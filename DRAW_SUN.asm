1e599600e37aa593e41b845a4682d0266ff62abd
Eduardo Lima Fabricio <edu.fabricio@gmail.com>
2020-04-28 00:54:10 -0300

    create a SUN

--- DRAW_SUN.asm
+++ DRAW_SUN.asm
@@ -1,46 +1,199 @@
+    processor 6502
 
-	processor 6502
-        include "vcs.h"
-        include "macro.h"
-        include "xmacro.h"
+    include "vcs.h"
+    include "macro.h"
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; Variables segment
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Start an unitialized segment at $80 for variable declaration
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    seg.u Variables
+    org $80
+P0Height ds 1       ; defines one byte for player 0 height
+P1Height ds 1       ; defines one byte for player 1 height
 
-        seg.u Variables
-	org $80
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Start out ROM code
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-Temp		.byte
+    seg Code
+    org $F000       ; defines the origin of the ROM at $F000
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; Code segment
+Reset:
+    CLEAN_START     ; Macro to safely clear the memory
 
-	seg Code
-        org $f000
+    ldx #$80        ; blue background color
+    stx COLUBK
 
-Start
-	CLEAN_START
+    lda #%1111      ; yellow playfield color
+    sta COLUPF      
 
-NextFrame
-        lsr SWCHB	; test Game Reset switch
-        bcc Start	; reset?
-; 1 + 3 lines of VSYNC
-	VERTICAL_SYNC
-; 37 lines of underscan
-	TIMER_SETUP 37
-        TIMER_WAIT
-; 192 lines of frame
-	TIMER_SETUP 192
-        TIMER_WAIT
-; 29 lines of overscan
-	TIMER_SETUP 29
-        TIMER_WAIT
-; total = 262 lines, go to next frame
-        jmp NextFrame
+    lda #10
+    sta P0Height
+    sta P1Height
+    
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; We set the TIA registers for the colors of P0 and P1
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
+    lda #$48	    ; player 0 color light red
+    sta COLUP0
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-; Epilogue
+    lda #$C6        ; player 1 color light green
+    sta COLUP1
+    
+    ldy #%00000010  ; CTRLPF D1 set to 1 means (score)
+    sty CTRLPF
+    
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Start a new frame by configuring VBLANK and VSYNC
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
+StartFrame:
+    lda #2
+    sta VBLANK
+    sta VSYNC
 
-	org $fffc
-        .word Start	; reset vector
-        .word Start	; BRK vector
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Generate the three scanlines of VSYNC
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
+    REPEAT 3
+      sta WSYNC
+    REPEND
+    
+    lda #0
+    sta VSYNC
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Let the TIA 37 scanlines of VBLANK
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
+    REPEAT 37
+      sta WSYNC
+    REPEND
+    
+    lda #0
+    sta VBLANK
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Draw the 192 visible scanlines
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
+VisibleScanlines:
+    ; Draw 10 empty scanlines at the top of the frame
+    REPEAT 10
+      sta WSYNC
+    REPEND
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Displays 10 scanlines for the scoreboad number.
+;; Pulls data from an array fo bytes defined at NumberBitmap.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
+    ldy #0
+ScoreboardLoop:
+    lda NumberBitmap,Y
+    sta PF1
+    sta WSYNC
+    iny
+    cpy #10
+    bne ScoreboardLoop
+    
+    lda #0
+    sta PF1 ; disable playfield
+
+    ; Draw 50 empty scanlines between scoreboard and player
+    REPEAT 50
+    	sta WSYNC
+    REPEND
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Displays 10 scanlines for the Player 0 graphics.
+;; Pulls data from an array fo bytes defined at NumberBitmap.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
+    ldy #0
+Player0Loop:
+    lda PlayerBitmap,Y
+    sta GRP0
+    sta WSYNC
+    iny
+    cpy P0Height
+    bne Player0Loop
+    
+    lda #0
+    sta GRP0 ; disable player 0 graphics
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Displays 10 scanlines for the Player 0 graphics.
+;; Pulls data from an array fo bytes defined at NumberBitmap.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
+    ldy #0
+Player1Loop:
+    lda PlayerBitmap,Y
+    sta GRP1
+    sta WSYNC
+    iny
+    cpy P1Height
+    bne Player1Loop
+    
+    lda #0
+    sta GRP1 ; disable player 0 graphics
+    
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Draw the remaining 102 scanlines (192-90), since we already
+;; used 10+10+50+10+10=80 scanlines in the current frame
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    REPEAT 102
+       sta WSYNC
+    REPEND
+   
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Output 30 more VBLANK overscan lines to complete out frame
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
+    REPEAT 30
+    	sta WSYNC
+    REPEND
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Loop Nex Frame
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
+
+    jmp StartFrame
+   
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Defines an array of bytes to draw the Player Sprite.
+;; We add these bytes in the last ROM addresses.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
+    org $FFE8
+PlayerBitmap:
+    .byte #%01111110
+    .byte #%11111111
+    .byte #%10011001
+    .byte #%11111111
+    .byte #%11111111
+    .byte #%11111111
+    .byte #%10111101
+    .byte #%11000011
+    .byte #%11111111
+    .byte #%01111110
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Defines an array of bytes to draw the scoreboard number.
+;; We add these bytes in the last ROM addresses.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
+    org $FFF2
+NumberBitmap:
+    .byte #%00001110
+    .byte #%00001110
+    .byte #%00000010
+    .byte #%00000010
+    .byte #%00001110
+    .byte #%00001110
+    .byte #%00001000
+    .byte #%00001000
+    .byte #%00001110
+    .byte #%00001110
+
+    
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Complete Rom Size 4KB
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+    org $FFFC
+    .word Reset
+    .word Reset
+
